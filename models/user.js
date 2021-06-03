f6f1deb51e27d3755fdbab656f47da1296215c43
jjiahuang <15602215864@163.com>
2021-06-03 11:53:01 +0800

    接入mongoose数据库模型

--- models/user.js
+++ models/user.js
@@ -1,5 +1,18 @@
 var mongodb = require('./db')
-var crypto = require('crypto');// 通过生成散列值加密密码
+var crypto = require('crypto'); // 通过生成散列值加密密码
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blog');
+
+var userSchema = new mongoose.Schema({
+    name: String,
+    password: String,
+    email: String,
+    head: String
+}, {
+    collection: 'users'
+});
+
+var userModel = mongoose.model('User', userSchema);
 
 function User(user) {
     this.name = user.name
@@ -7,61 +20,36 @@ function User(user) {
     this.email = user.email
 }
 
-module.exports = User
-
-// 存储用户信息
 User.prototype.save = function (callback) {
     var md5 = crypto.createHash('md5'),
-    email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
-    head = "http://www.gravatar.com/avatar/" +  email_MD5 + "?s=48"
+        email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+        head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
     var user = {
         name: this.name,
         password: this.password,
         email: this.email,
-        head:head
-    }
-    mongodb.open(function (err, db) {
+        head: head
+    };
+
+    var newUser = new userModel(user);
+
+    newUser.save(function (err, user) {
         if (err) {
-            return callback(err)
+            return callback(err);
         }
-        db.collection('users', function (err, collection) {
-            if (err) {
-                mongodb.close()
-                return callback(err)
-            }
-            collection.insert(user, {
-                safe: true
-            }, function (err, user) {
-                mongodb.close()
-                if (err) {
-                    return callback(err)
-                }
-                callback(null, user[0])
-            })
-        })
-    })
+        callback(null, user);
+    });
 };
 
-// 读取用户信息
 User.get = function (name, callback) {
-    mongodb.open(function (err, db) {
+    userModel.findOne({
+        name: name
+    }, function (err, user) {
         if (err) {
-            return callback(err)
+            return callback(err);
         }
-        db.collection('users', function (err, collection) {
-            if(err){
-                mongodb.close()
-                return callback(err)
-            }
-            collection.findOne({
-                name: name
-            }, function (err, user) {
-                mongodb.close()
-                if (err) {
-                    return callback(err)
-                }
-                callback(null, user)
-            })
-        })
-    })
-}
+        callback(null, user);
+    });
+};
+
+module.exports = User;
